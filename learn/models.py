c913a90d438ae6babe8be0d11ddec87483d76dcf
HanxiangLiu <hanxiangliu529@gmail.com>
2024-01-11 22:26:52 -0800

    Create database mapping class

--- learn/models.py
+++ learn/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    '''课程表映射类，包括课程名字、发布时间和学习人数等属性
+    '''
+
+    name = models.CharField(max_length=64)
+    pub_date = models.DateField()
+    stu_number = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f'<Course: {self.name}>'
