d7edf9606e4896fd4a13210caa9587b09d9b05f1
HanxiangLiu <hanxiangliu529@gmail.com>
2024-01-13 23:22:59 -0800

    Cookie and Session

--- learn/views.py
+++ learn/views.py
@@ -32,8 +32,18 @@ def add_a_b(request, a, b):
     return HttpResponse(a+b)
 
 def login(request, methods=['GET', 'POST']):
-    name = request.POST.get('name')
-    password = request.POST.get('password')
-    if name == 'shiyanlou' and password=='hello':
-        return HttpResponse('登录成功')
-    return render(request, 'learn/login.html')
+    if request.method == 'GET':
+        name = request.COOKIES.get('name')
+        sessionid = request.COOKIES.get('sessionid')
+        if request.session.exists(sessionid):
+            resp = HttpResponse('您已处于登录状态，{}'.format(name))
+            return resp
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        password = request.POST.get('password')
+        if name == 'shiyanlou' and password=='hello':
+            request.session['name'] = name
+            resp = HttpResponse('登录成功，{}'.format(name))
+            resp.set_cookie('name', name, 60 * 5, path='/learn')
+            return resp
+    return render(request, 'learn/login.html', {'name': name})
